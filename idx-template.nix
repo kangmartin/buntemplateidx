92f9b8d7a6091eb37065f3198dce9824cfc0b8c9
Martin Kang <martin.kang@efrei.net>
2024-09-28 02:03:05 +0200

    Refactor idx-template.nix: Remove unused code and update dependencies

--- idx-template.nix
+++ idx-template.nix
@@ -6,18 +6,14 @@
 
     mkdir "$out"
     mkdir -p "$out/.idx/"
+    mkdir -p "$out/src/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
     shopt -s dotglob; cp -r ${./dev}/* "$out"
-    
-    mkdir -p "$out/src"
-    
+    shopt -s dotglob; cp -r ${./devsrc}/* "$out/src/"
     bun init
     bun add @stricjs/router @stricjs/utils
     chmod -R +w "$out"
     chmod +rwx "$out/index.ts"
-    chmod +rwx "$out/main.routes.ts"
-    
-    cp ${./main.routes.ts} "$out/src/main.routes.ts"
-    
+    chmod +rwx "$out/src/main.routes.ts"
   '';
 }
